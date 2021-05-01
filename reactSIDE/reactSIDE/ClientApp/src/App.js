dc2970abf624f9c58b6e2b7c7718a24c2af55e2f
Damian Gul <damian.robert.gul@gmail.com>
2021-05-01 19:47:24 +0200

    Menu added

--- reactSIDE/reactSIDE/ClientApp/src/App.js
+++ reactSIDE/reactSIDE/ClientApp/src/App.js
@@ -1,23 +1,41 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { Component, useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Shop from "./components/Shop";
+import Dropdown from "./components/Dropdown";
 
 import "./custom.css";
 
-export default class App extends Component {
-  static displayName = App.name;
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
 
-  render() {
-    return (
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/shop" component={Shop} />
-        </Switch>
-      </Router>
-    );
-  }
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", hideMenu);
+
+    return () => {
+      window.removeEventListener("resize", hideMenu);
+    };
+  });
+
+  return (
+    <Router>
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/shop" component={Shop} />
+      </Switch>
+    </Router>
+  );
 }
--- reactSIDE/reactSIDE/ClientApp/src/components/Navbar.js
+++ reactSIDE/reactSIDE/ClientApp/src/components/Navbar.js
@@ -1,12 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
   return (
-    <nav>
-      <Link to={"/"}>GBshop</Link>
-      <Link to={"/"}>Strona Główna</Link>
-      <Link to={"/shop"}>Sklep</Link>
+    <nav
+      className="flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono"
+      role="navigation"
+    >
+      <Link to={"/"} className="pl-8">
+        GBshop
+      </Link>
+      <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </div>
+      <div className="pr-8 md:block hidden">
+        <Link to={"/"} className="p-4">
+          Home
+        </Link>
+        <Link to={"/shop"} className="p-4">
+          Shop
+        </Link>
+        <Link to={"/cart"} className="p-4">
+          Cart
+        </Link>
+        <Link to={"/aboutus"} className="p-4">
+          About us
+        </Link>
+        <Link to={"/contact"} className="p-4">
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 };
