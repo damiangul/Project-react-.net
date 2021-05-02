edec44f0a43666e6a5cc6f0e816099cb3466fa95
Damian Gul <damian.robert.gul@gmail.com>
2021-05-02 16:23:43 +0200

    Improved styling on home page

--- reactSIDE/reactSIDE/ClientApp/src/components/Footer.js
+++ reactSIDE/reactSIDE/ClientApp/src/components/Footer.js
@@ -10,7 +10,7 @@ import {
 
 export default function Footer() {
   return (
-    <footer className="flex border-t">
+    <footer className="flex border-t p-3">
       <div className="w-3/4">
         <div className="pl-8 font-mono font-bold text-2xl">GBshop</div>
         <div>
@@ -27,13 +27,13 @@ export default function Footer() {
       </div>
       <div className="flex justify-around items-center w-1/4">
         <a href="https://pl-pl.facebook.com/theweeknd">
-          <FontAwesomeIcon icon={faFacebook} />
+          <FontAwesomeIcon className="text-3xl" icon={faFacebook} />
         </a>
         <a href="https://www.instagram.com/theweeknd/">
-          <FontAwesomeIcon icon={faInstagram} />
+          <FontAwesomeIcon className="text-3xl" icon={faInstagram} />
         </a>
         <a href="https://github.com/Krokus-lab/Project-react-.net">
-          <FontAwesomeIcon icon={faGithub} />
+          <FontAwesomeIcon className="text-3xl" icon={faGithub} />
         </a>
       </div>
     </footer>
