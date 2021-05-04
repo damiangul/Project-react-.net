6ae0c44c38767786afede099b7769ef3ec4793cd
Damian Gul <damian.robert.gul@gmail.com>
2021-05-04 20:57:32 +0200

    Fixed LogoutButton component

--- reactSIDE/reactSIDE/ClientApp/src/components/LogoutButton.js
+++ reactSIDE/reactSIDE/ClientApp/src/components/LogoutButton.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { loggingUser } from "../redux/loggedUserActions";
+import { useHistory } from "react-router-dom";
 
 export default function LogoutButton() {
   const dispatch = useDispatch();
+  let history = useHistory();
+
   const logoutUser = () => {
     dispatch(loggingUser("", null));
+    history.push("/");
   };
 
   return (
